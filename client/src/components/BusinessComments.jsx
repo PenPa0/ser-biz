f065e97c66dafa45b84c5022e4b6a05c563a37c7
PenPa0 <ppendon.code@gmail.com>
2023-05-29 15:34:26 +0800

    ✨Add comments function

--- client/src/components/BusinessComments.jsx
+++ client/src/components/BusinessComments.jsx
@@ -1,6 +1,34 @@
 import React from "react";
+import { useContext, useState, useEffect } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const BusinessComments = () => {
+  const auth = useContext(AuthContext);
+  const [comment, setComment] = useState({});
+  const [approvedComment, setApprovedComment] = useState([]);
+  let { businessId } = useParams(); //To pass in the business Id of the page
+
+  const addComment = async (e) => {
+    e.preventDefault();
+    console.log(comment, " HEY IM COMMENT CONSOLE LOG");
+    // setComment({ ...comment, business_id: businessId });
+    const response = await axios.post("/comment", comment, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+      },
+    });
+    // localStorage.setItem("businessId", response.data.businessId);
+  };
+  const displayComment = async () => {
+    const response = await axios.get(`/get-comments/${businessId}`);
+    console.log(response, "HEY IM DISPLAY COMMENT RESPONSE");
+    setApprovedComment(response.data);
+  };
+  useEffect(() => {
+    displayComment();
+  }, []);
   return (
     <div>
       <div className="w-full flex justify-center items-center">
@@ -17,38 +45,45 @@ const BusinessComments = () => {
           <div class="flex justify-between items-center mb-6">
             <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white"></h2>
           </div>
-          <form class="mb-6">
-            <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-              <label for="comment" class="sr-only">
-                Your comment
-              </label>
-              <textarea
-                id="comment"
-                rows="6"
-                class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                placeholder="Write a comment..."
-                required
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-            >
-              Post comment
-            </button>
-          </form>
+          {auth?.user && (
+            <form onSubmit={addComment} class="mb-6">
+              <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                <label for="comment" class="sr-only">
+                  Your comment
+                </label>
+                <textarea
+                  onChange={(e) => {
+                    let value = e.target.value;
+
+                    setComment({
+                      ...comment,
+                      comment: value,
+                      business_id: businessId,
+                    });
+                  }}
+                  id="comment"
+                  rows="6"
+                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                  placeholder="Write a comment..."
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+              >
+                Post comment
+              </button>
+            </form>
+          )}
           <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
             <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <p class="inline-flex items-center mr-3 text-sm text-neutral-200">
                   Michael Gough
                 </p>
                 <p class="text-sm text-gray-600 dark:text-gray-400">
-                  <time
-                    pubdate
-                    datetime="2022-02-08"
-                    title="February 8th, 2022"
-                  >
+                  <time pubdate datetime="" title="">
                     Feb. 8, 2022
                   </time>
                 </p>
@@ -107,10 +142,10 @@ const BusinessComments = () => {
               </div>
             </footer>
             <p class="text-gray-500 dark:text-gray-400">
-              Very straight-to-point article. Really worth time reading. Thank
+              {/* Very straight-to-point article. Really worth time reading. Thank
               you! But tools are just the instruments for the UX designers. The
               knowledge of the design tools are as important as the creation of
-              the design strategy.
+              the design strategy. */}
             </p>
             <div class="flex items-center mt-4 space-x-4">
               <button
@@ -139,12 +174,8 @@ const BusinessComments = () => {
           <article class="p-6 mb-6 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900">
             <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img
-                    class="mr-2 w-6 h-6 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="Jese Leos"
-                  />
+                <p class="inline-flex items-center mr-3 text-sm text-neutral-200 dark:text-white">
+                  Jese Leos
                 </p>
                 <p class="text-sm text-gray-600 dark:text-gray-400">
                   <time
@@ -236,114 +267,120 @@ const BusinessComments = () => {
               </button>
             </div>
           </article>
-          <article class="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-            <footer class="flex justify-between items-center mb-2">
-              <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img
+          {approvedComment.map((viewComment) => {
+            return (
+              console.log(viewComment, "HI IM APPROVED COMMENT"),
+              (
+                <article class="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                  <footer class="flex justify-between items-center mb-2">
+                    <div class="flex items-center">
+                      <p class="inline-flex items-center mr-3 text-sm text-neutral-200 dark:text-white">
+                        {/* <img
                     class="mr-2 w-6 h-6 rounded-full"
                     src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
                     alt="Bonnie Green"
-                  />
-                  Bonnie Green
-                </p>
-                <p class="text-sm text-gray-600 dark:text-gray-400">
-                  <time pubdate datetime="2022-03-12" title="March 12th, 2022">
-                    Mar. 12, 2022
-                  </time>
-                </p>
-              </div>
-              <button
-                id="dropdownComment3Button"
-                data-dropdown-toggle="dropdownComment3"
-                class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                type="button"
-              >
-                <svg
-                  class="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                </svg>
-                <span class="sr-only">Comment settings</span>
-              </button>
-              {/* <!-- Dropdown menu --> */}
-              <div
-                id="dropdownComment3"
-                class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul
-                  class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                  aria-labelledby="dropdownMenuIconHorizontalButton"
-                >
-                  <li>
-                    <a
-                      href="#"
-                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  /> */}
+                        {viewComment.user_email}
+                      </p>
+                      <p class="text-sm text-gray-600 dark:text-gray-400">
+                        <time
+                          pubdate
+                          datetime="2022-03-12"
+                          title="March 12th, 2022"
+                        >
+                          {viewComment.created_at}
+                        </time>
+                      </p>
+                    </div>
+                    <button
+                      id="dropdownComment3Button"
+                      data-dropdown-toggle="dropdownComment3"
+                      class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                      type="button"
                     >
-                      Edit
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      <svg
+                        class="w-5 h-5"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                      </svg>
+                      <span class="sr-only">Comment settings</span>
+                    </button>
+                    {/* <!-- Dropdown menu --> */}
+                    <div
+                      id="dropdownComment3"
+                      class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
                     >
-                      Remove
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#"
-                      class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      <ul
+                        class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                        aria-labelledby="dropdownMenuIconHorizontalButton"
+                      >
+                        <li>
+                          <a
+                            href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Edit
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Remove
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="#"
+                            class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          >
+                            Report
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </footer>
+                  <p class="text-gray-500 dark:text-gray-400">
+                    {viewComment.comment}
+                  </p>
+                  <div class="flex items-center mt-4 space-x-4">
+                    <button
+                      type="button"
+                      class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
                     >
-                      Report
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </footer>
-            <p class="text-gray-500 dark:text-gray-400">
-              The article covers the essentials, challenges, myths and stages
-              the UX designer should consider while creating the design
-              strategy.
-            </p>
-            <div class="flex items-center mt-4 space-x-4">
-              <button
-                type="button"
-                class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-              >
-                <svg
-                  aria-hidden="true"
-                  class="mr-1 w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                  ></path>
-                </svg>
-                Reply
-              </button>
-            </div>
-          </article>
+                      <svg
+                        aria-hidden="true"
+                        class="mr-1 w-4 h-4"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                        ></path>
+                      </svg>
+                      Reply
+                    </button>
+                  </div>
+                </article>
+              )
+            );
+          })}
+
           <article class="p-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
             <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img
-                    class="mr-2 w-6 h-6 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
-                    alt="Helene Engels"
-                  />
+                <p class="inline-flex items-center mr-3 text-sm text-neutral-200 text-gray-900 dark:text-white">
+                  Helene Engels
                 </p>
                 <p class="text-sm text-gray-600 dark:text-gray-400">
                   <time pubdate datetime="2022-06-23" title="June 23rd, 2022">
