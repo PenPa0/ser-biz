978caae2bb446ad1fa16ef9e25cd0022e77c694c
PenPa0 <ppendon.code@gmail.com>
2023-05-30 21:17:44 +0800

    🚸Improve dropdown, Clicking outside closes it.

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
-
+import { useState, useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 const Comments = (props) => {
   const [isHidden, setIsHidden] = useState(true);
-
+  const auth = useContext(AuthContext);
+  let menuRef = useRef();
   const handleDropdown = () => {
     setIsHidden(false);
   };
   const hideDropdown = () => {
     setIsHidden(true);
   };
+
+  useEffect(() => {
+    let handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setIsHidden(true);
+        console.log(menuRef.current);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
   return (
     <article class="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+      {console.log(auth.user, "HI IM AUTH USER AT COMMENTS")}
       <footer class="flex justify-between items-center mb-2">
         <div class="flex items-center">
           <p class="inline-flex items-center mr-3 text-sm text-neutral-200 dark:text-white">
@@ -27,62 +43,74 @@ const Comments = (props) => {
             </time>
           </p>
         </div>
-        {isHidden ? (
-          <button
-            onClick={handleDropdown}
-            id="dropdownCommentButton"
-            data-dropdown-toggle="dropdownComment"
-            class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            type="button"
-          >
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-            </svg>
-            <span class="sr-only">Comment settings</span>
-          </button>
-        ) : (
-          /* <!-- Dropdown menu --> */
-          <div
-            id="dropdownComment"
-            class="{}hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-          >
-            <ul
-              class="py-1 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdownMenuIconHorizontalButton"
-            >
-              <li>
-                <a
-                  onClick={hideDropdown}
-                  //   href="#"
-                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+        {auth?.user ? (
+          <div>
+            {isHidden ? (
+              <button
+                onClick={handleDropdown}
+                id="dropdownCommentButton"
+                data-dropdown-toggle="dropdownComment"
+                class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                type="button"
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Edit
-                </a>
-              </li>
-              <li>
-                <a
-                  //   href="#"
-                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                </svg>
+                <span class="sr-only">Comment settings</span>
+              </button>
+            ) : (
+              /* <!-- Dropdown menu --> */
+
+              <div
+                ref={menuRef}
+                id="dropdownComment"
+                class="{}hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  class="py-1 text-sm text-gray-700 dark:text-gray-200"
+                  aria-labelledby="dropdownMenuIconHorizontalButton"
                 >
-                  Remove
-                </a>
-              </li>
-              <li>
-                <a
-                  //   href="#"
-                  class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Report
-                </a>
-              </li>
-            </ul>
+                  {auth.user.user_id === props.viewComment.user_id && ( //&& means if Condition is true Render my block, if its false render nothing.
+                    <>
+                      <li>
+                        <a
+                          onClick={hideDropdown}
+                          //   href="#"
+                          className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Edit
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          //   href="#"
+                          className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        >
+                          Remove
+                        </a>
+                      </li>
+                    </>
+                  )}
+                  <li>
+                    <a
+                      //   href="#"
+                      className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Report
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
+        ) : (
+          ""
         )}
       </footer>
       <p class="text-gray-500 dark:text-gray-400">
