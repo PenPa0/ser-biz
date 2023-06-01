57111fc5e3c37a391d5c37ba3360305b45c7a90f
PenPa0 <ppendon.code@gmail.com>
2023-06-01 16:24:13 +0800

    ⚡️Improve persistent login on reload to specific pages

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import CreateBusiness from "./pages/CreateBusiness";
 import { AuthProvider, AuthContext } from "./contexts/AuthContext";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import AdminPage from "./pages/AdminPage";
 import axios from "axios";
 import SearchPage from "./pages/SearchPage";
@@ -20,6 +20,7 @@ import SearchPage from "./pages/SearchPage";
 function App() {
   const auth = useContext(AuthContext);
   const { user, setUser } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (typeof user == "undefined") {
       const token = localStorage.getItem("jwt_token");
@@ -47,19 +48,39 @@ function App() {
               "HI IM FROM ERROR RESPONSE IF STATEMENT IN APP JS"
             );
             console.log("Not logged in");
+          })
+          .finally(() => {
+            setIsLoading(false); // Set loading to false when the verification is complete
           });
+      } else {
+        setIsLoading(false); // If there's no token, set loading to false
       }
-    }
+    } else {
+      setIsLoading(false);
+    } // If the user is already defined, set loading to false
   }, [user]);
 
+  if (isLoading) {
+    return <div>LOADING......</div>;
+  }
   return (
     <div className="App">
       {/* {console.log(user, "HI IM USER FROM APP JS SETSTATE")} */}
-      {/* <div className="h-[30px] bg-red-800">{auth?.user?.email}</div> */}
+      <div className="h-[30px] bg-red-800">{auth?.user?.email}</div>
+      <div className="h-[30px] bg-red-800">{auth?.user?.role}</div>
       {/* for testing and seeing if user is logged in */}
       <ToastContainer />
       <Routes>
-        <Route path="/AdminPage" element={<AdminPage />} />
+        <Route
+          path="/AdminPage"
+          element={
+            auth?.user?.role === "admin" ? (
+              <AdminPage />
+            ) : (
+              <Navigate to="/Sign-up" />
+            )
+          }
+        />
         <Route path="/" element={<HomePage />} />
         <Route path="/Business/:businessId" exact element={<BusinessPage />} />
         <Route path="/About" element={<AboutPage />} />
